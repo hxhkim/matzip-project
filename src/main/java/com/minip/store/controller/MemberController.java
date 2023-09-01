85b15da09cfa000c64cae09a4965380c4c0642b5
hxhkim <hahyun.h.kim@gmail.com>
2023-09-01 17:31:50 +0900

    로그아웃 버튼 스타일 수정

--- src/main/java/com/minip/store/controller/MemberController.java
+++ src/main/java/com/minip/store/controller/MemberController.java
@@ -1,5 +1,5 @@
 package com.minip.store.controller;
-// hi
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
